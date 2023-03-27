0ed43e746d6a0a45cfe11e1f0acd949d74cb5351
Avdhut Pailwan <avdhutpailwan@gmail.com>
2023-03-27 18:58:29 +0530

    renamed registeration -> registration for the submission cause it got rejected 2nd time

--- http-server/index.js
+++ http-server/index.js
@@ -23,7 +23,7 @@ fs.readFile('project.html', (err, project) => {
   projectContent = project
 })
 
-fs.readFile('registeration.html', (err, register) => {
+fs.readFile('registration.html', (err, register) => {
   if (err) throw err
   registerPage = register
 })
