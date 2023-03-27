0448842cda99df22f963e06e0d87c2b4b9e5388b
Avdhut Pailwan <avdhutpailwan@gmail.com>
2023-03-27 16:49:41 +0530

    Removed sample.txt Installed minimist with "npm install minimist" to take multiple arguments Added demo program from the wd201 level 2 lesson "Accepting command line arguments" which uses the minimist

--- http-server/index.js
+++ http-server/index.js
@@ -1,19 +1,59 @@
-const http = require('http')
-const fs = require('fs')
+// const http = require('http')
+// const fs = require('fs')
 
-// Create an HTTP server
-const server = http.createServer((_req, res) => {
-  // Create a readable stream to read the contents of the file
-  const stream = fs.createReadStream('sample.txt')
-  // Pipe the contents of the file to the response stream
-  stream.pipe(res)
+// // Create an HTTP server
+// const server = http.createServer((_req, res) => {
+//   // Create a readable stream to read the contents of the file
+//   const stream = fs.createReadStream('sample.txt')
+//   // Pipe the contents of the file to the response stream
+//   stream.pipe(res)
 
-  // Alternatively, you can use fs.readFile() to read the contents of the file
-  // and send it as the response using res.end()
-  // fs.readFile('sample.txt', (_err, data) => {
-  //   res.end(data)
-  // })
-})
+//   // Alternatively, you can use fs.readFile() to read the contents of the file
+//   // and send it as the response using res.end()
+//   // fs.readFile('sample.txt', (_err, data) => {
+//   //   res.end(data)
+//   // })
+// })
 
-// Listen on port 3000
-server.listen(3000)
+// // Listen on port 3000
+// server.listen(3000)
+
+// ------------------------------------------------------------------------------------------------
+
+// // Import the 'readline' module
+// const readline = require('readline')
+
+// // Create a readline interface for reading input from the console
+// const lineDetail = readline.createInterface({
+//   input: process.stdin, // Set the input stream to 'stdin' (standard input)
+//   output: process.stdout // Set the output stream to 'stdout' (standard output)
+// })
+
+// // Ask the user for their name and wait for their input
+// lineDetail.question('Please provide your name - ', (name) => {
+//   // Once the user provides their name, print a greeting message with their name
+//   console.log(`Hi ${name}!`)
+
+//   // Close the readline interface to stop waiting for input
+//   lineDetail.close()
+// })
+
+// const args = require('minimist')(process.argv.slice(2))
+
+// console.log(args)
+
+// const args = require('minimist')(process.argv.slice(2), {
+//   alias: {
+//     n: 'name',
+//     a: 'age'
+//   }
+// })
+
+// console.log(args)
+
+// const args = require('minimist')(process.argv.slice(2), {
+//   default: {
+//     greeting: 'Hello'
+//   }
+// })
+// console.log(args)
