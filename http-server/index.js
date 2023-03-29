1489895dfb7f86426382f9db585b7d1f6b134516
Avdhut Pailwan <avdhutpailwan@gmail.com>
2023-03-30 01:13:41 +0530

    Added comments to web app 'index.js' so that reader can understand the code

--- http-server/index.js
+++ http-server/index.js
@@ -1,18 +1,25 @@
+// Require the 'http' and 'fs' modules
 const http = require('http')
 const fs = require('fs')
+
+// Require the 'minimist' module and parse command-line arguments
 const args = require('minimist')(process.argv.slice(1), {
+  // Define an alias for the 'port' argument
   alias: {
     p: 'port'
   },
+  // Set the default value for the 'port' argument
   default: {
     port: 3000
   }
 })
 
+// Define variables to hold the content of each HTML page
 let homeContent = ''
 let projectContent = ''
 let registerPage = ''
 
+// Read the contents of each HTML page into its respective variable
 fs.readFile('home.html', (err, home) => {
   if (err) throw err
   homeContent = home
@@ -28,19 +35,28 @@ fs.readFile('registration.html', (err, register) => {
   registerPage = register
 })
 
+// Create an HTTP server that listens for requests on the specified port
 http.createServer((request, response) => {
+  // Get the requested URL
   const url = request.url
+
+  // Set the response header to indicate that the response contains HTML
   response.writeHeader(200, { 'Content-Type': 'text/html' })
+
+  // Use a switch statement to handle different URLs
   switch (url) {
     case '/project':
+      // If the URL is '/project', send the project content
       response.write(projectContent)
       response.end()
       break
     case '/registration':
+      // If the URL is '/registration', send the registration page content
       response.write(registerPage)
       response.end()
       break
     default:
+      // For any other URL, send the home page content
       response.write(homeContent)
       response.end()
       break
