9be1e5373357190982e80c103de5aa034194ea66
Avdhut Pailwan <avdhutpailwan@gmail.com>
2023-04-26 15:41:42 +0530

    Created todo-app for level 6 successfully installed express and ran it

---
+++ todo-app/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+  response.send('hello world')
+})
+
+app.listen(3000, () => {
+  console.log('started express server server at port 3000')
+})
